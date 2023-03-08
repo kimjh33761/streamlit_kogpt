4ed8da151ebbc3c564e8c17a4c8bf3f0b42342da
kimjunghyun2 <jhkim5381@gmail.com>
2023-03-08 15:27:36 +0900

    Changes

--- app.py
+++ app.py
@@ -14,13 +14,17 @@ idxE = 0
 max_tokens = 64
 # 필수 파라미터
 
-
-
 st.title('hello world')
 prompt = st.text_input('대화를 입력하세요','여기에 입력하면 됩니다.')
 if((prompt == "종료")|(prompt == "end")|(prompt == "끝")):
     st.write('대화를 종료할게요')
-result = api.generate(prompt, max_tokens, temperature=0.7, top_p=0.8)
+token = st.slider(" 최대문장 길이를 지정하세요",1,1000,64)
+temp = st.slider("강도를 지정하세요",0.0,1.0,0.7)
+# temper 지정
+tp = st.slider("top_p를 지정하세요",0.0,1.0,0.8)
+# top_p 지정
+#option 근데 이거 너무 복잡해
+result = api.generate(prompt, token, temperature=temp, top_p=tp)
 gen = result['generations']
 finres = gen[0]['text']
 st.write(finres)
